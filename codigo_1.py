72ab16cef34425b697e4013bb68a8002497a21a1
erickdavid186 <erickdavid14a@gmail.com>
2026-04-20 07:52:36 -0600

    Add files via upload

---
+++ codigo_1.py
@@ -0,0 +1,22 @@
+#Método de  Ordenamiento la burbuja ascendente (BubbleSort)
+from random import sample
+lista = list(range(100))
+vectorbs = sample(list,8)
+def bubblesort (vectorbs):
+    """Esta función ordena el vector que le pases como argumento con el Método de Bubble Sort"""
+
+print("El vector a orden ees:",vectorbs)
+n=0
+
+ for_in vectorbs:
+  n+=1
+
+ for i in range(n-1):
+  
+     for j in range(0, n-i-1):
+
+         if vectorbs[j]> vectorbs[j+1] :
+             vectorbs[j],vectorbs[j+1]= vectorbs[j+1], vectorbs[j]
+
+    print ("El vector ordenado es:",vectorbs)
+bubblesort(vectorbs)
