92508ad2112ce30db65a1ebe8fa5968861e9741c
erickdavid186 <erickdavid14a@gmail.com>
2026-04-30 07:31:33 -0600

    Add files via upload

--- codigo 2.py
+++ codigo 2.py
@@ -1,27 +1,44 @@
-#Metodo de Ordenamiento de Selección (SelecciónShort)
+#Metodo de ordenamiento de seleccion (selectionSort)
+# Importa la función sample del módulo random
+# Sirve para obtener una muestra aleatoria de elementos
+from random import sample
 
-from random import sample 
-
+# Crea una lista con números del 0 al 99
 lista = list(range(100))
 
-vectorselect = sample(lista,8)
+# Genera un vector con 8 números aleatorios sin repetir
+vectorselect = sample(lista, 8)
 
+# Definición de la función selectionsort
 def selectionsort(vectorselect):
-    """"Esta función ordenara el vector que la pases como argumento con el Método Selección Sort"""
+    """
+    Esta función ordena el vector usando el método de selección (Selection Sort)
+    """
 
-    print ("El vector a ordenar es:",vectorselect)
+    # Muestra el vector original
+    print("El vector a ordenar es:", vectorselect)
 
-    largo = 0
+    # Calcula la longitud del vector
+    largo = len(vectorselect)
 
-    for _ in vectorselect:
-        largo += 1
-
+    # Recorre cada posición del vector
     for i in range(largo):
 
+        # Asume que el elemento actual es el mínimo
         minimo = i
-        for j in range(i+1, largo):
-            if vectorselect [minimo] > vectorselect[j]:
-                minimo = j
+
+        # Busca el elemento más pequeño en el resto del vector
+        for j in range(i + 1, largo):
+            if vectorselect[minimo] > vectorselect[j]:
+                minimo = j  # Actualiza la posición del mínimo
+
+        # Intercambia el elemento actual con el mínimo encontrado
         vectorselect[i], vectorselect[minimo] = vectorselect[minimo], vectorselect[i]
 
-        print("el vector ordenado es: ",vectorselect)
+    # Muestra el vector ya ordenado
+    print("El vector ordenado es:", vectorselect)
+
+
+# Llama a la función para ordenar el vector
+selectionsort(vectorselect)
+                    
